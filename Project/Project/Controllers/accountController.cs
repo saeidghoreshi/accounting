75fa76f5c8a058a34288b18d7b35e561f525eb61
saeidghoreshi <Ryan.goreshi@yahoo.com>
2013-03-27 13:46:06 -0700

    update

--- Project/Project/Controllers/accountController.cs
+++ Project/Project/Controllers/accountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,7 @@ namespace Project.Controllers
             {
                 account = ctx.accounts.Where(x => x.catTypeID == catTypeID && x.ownerEntityID == entityID && x.currencyID == currencyID).SingleOrDefault();
             }
+
             return account;
         }
     }
