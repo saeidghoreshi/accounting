b123c10a41c1b0aad009e4d3de54fdd8545bf35e
Ryan Goreshi <ryan.goreshi@yahoo.com>
2013-03-30 00:04:28 -0700

    update

---
+++ Project/Project/Classes/Transfer/InternalTransfer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Transactions;
+
+using Project.Models;
+
+namespace Project.Structure
+{   
+    public class InternalTransfer: Transfer
+    {
+        public internalTransfer INTERNALTSFR { get; set; }
+        public const projectEnums.transferType TRANSFERTYPE = projectEnums.transferType.Internal;
+
+        public InternalTransfer() { }
+        public InternalTransfer(int transferID)
+        {
+            this.Load(transferID);
+        }
+
+        public void New(int issuerEntityID, int receiverEntityID, decimal amount, int currencyID)
+        {
+            using (var ctx = new accountingEntities())
+            using (var ts = new TransactionScope())
+            {
+                base.TRANSFER = new transfer()
+                {
+                    issuerEntityID = issuerEntityID,
+                    receiverEntityID = receiverEntityID,
+                    amount = amount,
+                    currencyID = currencyID,
+                    transferTypeID = (int)TRANSFERTYPE
+                };
+                ctx.transfers.AddObject(TRANSFER);
+                
+                INTERNALTSFR= new internalTransfer{ID = TRANSFER.ID};
+
+                ctx.internalTransfers.AddObject(INTERNALTSFR);
+                ctx.SaveChanges();
+
+                ts.Complete();
+            }
+        }
+        public void Load(int transferID)
+        {
+            using (var ctx = new accountingEntities())
+            {
+                INTERNALTSFR= ctx.internalTransfers
+                    .Where(x => x.ID == transferID)
+                    .SingleOrDefault();
+
+                if (INTERNALTSFR == null)
+                    throw new Exception("no such a EXT Payment Exists");
+            }
+        }
+    }
+}
